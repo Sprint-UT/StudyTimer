f9091fb71c51392bb3de193a4b8612b4f71082d5
茂山 丈太郎 <jojojoe77@gmail.com>
2015-04-25 13:47:58 +0900

    The first commit

---
+++ StudyTimer/StudyTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyTimer
+//
+//  Created by 茂山 丈太郎 on 2015/04/25.
+//  Copyright (c) 2015年 茂山 丈太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
