fae9131e097333a2f4f808e000f3ccecdf347c38
Daisuke Shibato <Daisuke@MyMacBookAir2013Mid.local>
2015-04-25 15:59:07 +0900

    first commit by Shibato

---
+++ TestTime/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TestTime
+//
+//  Created by Daisuke Shibato on 2015/04/25.
+//  Copyright (c) 2015年 Daisuke Shibato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var TestBotton: UIButton!
+    
+    @IBOutlet var TestLabel: UILabel!
+    
+    
+
+    @IBAction func TapBotton(sender: AnyObject) {
+        
+        TestLabel.text = "OK"
+    }
+}
+
